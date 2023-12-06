3124f74bcc4cce93ae5a9e634968e3b2b91c8bad
Dmytro Semenko <45269867+DmytroSemenko@users.noreply.github.com>
2023-12-07 01:40:13 +0200

    Add TelegarmMiniAppAuthenticationExtensions

---
+++ Telegram.MiniApp.Authentication/Extensions/TelegarmMiniAppAuthenticationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Telegram.MiniApp.Authentication.Extensions;
+
+public static class TelegarmMiniAppAuthenticationExtensions
+{
+    public static AuthenticationBuilder AddTelegarmMiniAppAuthentication(this AuthenticationBuilder authenticationBuilder, string botToken, TimeSpan? expirationDuration)
+    {
+        authenticationBuilder
+            .AddScheme
+            <TelegarmMiniAppAuthenticationOptions,
+            TelegarmMiniAppAuthenticationHandler>
+            (TelegarmMiniAppAuthenticationConstants.SchemeName,
+            options =>
+        {
+            options.BotToken = botToken ?? throw new ArgumentNullException(nameof(botToken), $"Bot token value is null.");
+            if (expirationDuration.HasValue)
+            {
+                options.ExpirationDuration = expirationDuration.Value;
+            }
+        });
+
+        return authenticationBuilder;
+    }
+}
--- Telegram.MiniApp.Authentication/TelegarmMiniAppAuthenticationConstants.cs
+++ Telegram.MiniApp.Authentication/TelegarmMiniAppAuthenticationConstants.cs
@@ -2,7 +2,7 @@
 
 public static class TelegarmMiniAppAuthenticationConstants
 {
-    public const string SchemaName = "TelegarmMiniAppAuthentication";
+    public const string SchemeName = "TelegarmMiniAppAuthentication";
     internal const string Header = "Authentication";
     internal const string HashKey = "hash";
     internal const string AuthDateKey = "auth_date";
